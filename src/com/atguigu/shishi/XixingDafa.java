d3be609b2373ad390bb37479f56950539887fceb
martinjuly <541490026@qq.com>
2020-05-13 02:18:30 +0800

    更新1

--- src/com/atguigu/shishi/XixingDafa.java
+++ src/com/atguigu/shishi/XixingDafa.java
@@ -5,6 +5,7 @@ public class XixingDafa {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("1111111111111111111");
-	}
+
+System.out.println("11111111111111111112");}
 
 }
